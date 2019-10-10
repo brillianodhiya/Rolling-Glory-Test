8910655a12886da2de8b124ababc17883be25407
brillianodhiya <brillidhiya@gmail.com>
2019-10-10 10:41:01 +0700

    update config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  // mode: 'spa',
   /*
   ** Headers of the page
   */
@@ -40,29 +41,29 @@ module.exports = {
           loader: 'eslint-loader',
           exclude: /(node_modules)/
         })
-        const vueLoader = config.module.rules.find(
-          ({loader}) => loader === 'vue-loader')
-        const { options: {loaders} } = vueLoader || { options: {} }
-        if (loaders) {
-          for (const loader of Object.values(loaders)) {
-            changeLoaderOptions(Array.isArray(loader) ? loader : [loader])
-          }
-        }
-        config.module.rules.forEach(rule => changeLoaderOptions(rule.use))
+        // const vueLoader = config.module.rules.find(
+        //   ({loader}) => loader === 'vue-loader')
+        // const { options: {loaders} } = vueLoader || { options: {} }
+        // if (loaders) {
+        //   for (const loader of Object.values(loaders)) {
+        //     changeLoaderOptions(Array.isArray(loader) ? loader : [loader])
+        //   }
+        // }
+        // config.module.rules.forEach(rule => changeLoaderOptions(rule.use))
         // console.log(util.inspect(config.module.rules, { depth: 6 }))
       }
     }
   }
 }
 
-function changeLoaderOptions (loaders) {
-  if (loaders) {
-    for (const loader of loaders) {
-      if (loader.loader === 'sass-loader') {
-        Object.assign(loader.options, {
-          includePaths: ['./assets']
-        })
-      }
-    }
-  }
-}
+// function changeLoaderOptions (loaders) {
+//   if (loaders) {
+//     for (const loader of loaders) {
+//       if (loader.loader === 'sass-loader') {
+//         Object.assign(loader.options, {
+//           includePaths: ['./assets']
+//         })
+//       }
+//     }
+//   }
+// }
